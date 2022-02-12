c7af01bf5839558437d411763a77ed22cba8d41a
Xiang Zhu <zhuxiang1212@hotmail.com>
2022-02-12 16:27:02 +0800

    [TEST]: Nuget Dependency Test

---
+++ KaerMorhen.SDK.Greeting.Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KaerMorhen.SDK.Greeting.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GreetingService.SayHello("U2"));
+            Console.ReadLine();
+        }
+    }
+}
