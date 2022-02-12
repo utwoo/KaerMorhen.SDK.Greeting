5a05a101f88f50fa7e04dacf98c8a24df899e8da
Xiang Zhu <zhuxiang1212@hotmail.com>
2022-02-12 13:35:02 +0800

    [Test] Artifacts

---
+++ KaerMorhen.SDK.Greeting/GreetingService.cs
@@ -0,0 +1,10 @@
+﻿namespace KaerMorhen.SDK.Greeting
+{
+    public class GreetingService
+    {
+        public static string SayHello(string name)
+        {
+            return $"Hello World, {name}";
+        }
+    }
+}
